c1526689c6d5436e5ba0c811b876fdfc4068b532
Tziporah <tziporawerzberger@gmail.com>
2020-10-20 15:30:43 -0400

    Initial commit

---
+++ store_item.rb
@@ -0,0 +1,6 @@
+shirts = { color: :red, price: 20, sale: :false }
+pants = { color: :denim, price: 25, sale: :false }
+coats = { color: :white, price: 75, sale: :true }
+puts "#{shirts[:color]} shirts cost $#{shirts[:price]}."
+puts "#{pants[:color]} pants cost $#{pants[:price]}."
+puts "#{coats[:color]} coats cost $#{coats[:price]}."
