4d47ec4412ab7ea5ef7a43b69160b25f6d9ed0b6
Tziporah <tziporawerzberger@gmail.com>
2020-10-21 13:41:50 -0400

    makes input into hash

--- store_item.rb
+++ store_item.rb
@@ -8,11 +8,11 @@
 class Store
   attr_accessor :item, :color, :sale, :price
 
-  def initialize(item, color, price, sale)
-    @item = item
-    @color = color
-    @price = price
-    @sale = sale
+  def initialize(input_options)
+    @item = input_options[:item]
+    @color = input_options[:color]
+    @price = input_options[:price]
+    @sale = input_options[:sale]
   end
 
   def info
@@ -20,9 +20,9 @@ class Store
   end
 end
 
-item1 = Store.new("shirts", "red", 20, false)
-item2 = Store.new("pants", "denim", 25, false)
-item3 = Store.new("coats", "white", 75, true)
+item1 = Store.new(item: "shirts", color: "red", price: 20, sale: false)
+item2 = Store.new(item: "pants", color: "denim", price: 25, sale: false)
+item3 = Store.new(item: "coats", color: "white", price: 75, sale: true)
 item2.price = 15
 puts item1.color
 puts item1.sale
