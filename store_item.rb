2ab2667deb49329b43538eeb3f9cca6ea5ca7087
Tziporah <tziporawerzberger@gmail.com>
2020-10-21 14:59:13 -0400

    adds inheritance to store

--- store_item.rb
+++ store_item.rb
@@ -20,10 +20,21 @@ class Store
   end
 end
 
+class Food < Store
+  attr_accessor :shelf_life
+
+  def initialize(input_options)
+    super
+    @shelf_life = input_options[:shelf_life]
+  end
+end
+
 item1 = Store.new(item: "shirts", color: "red", price: 20, sale: false)
 item2 = Store.new(item: "pants", color: "denim", price: 25, sale: false)
 item3 = Store.new(item: "coats", color: "white", price: 75, sale: true)
+item4 = Food.new(item: "ice_cream", color: "vanilla", price: 5, sale: false, shelf_life: "1 day")
 item2.price = 15
 puts item1.color
 puts item1.sale
 puts item1.item
+puts item4.shelf_life
